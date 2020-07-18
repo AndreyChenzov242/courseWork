0b1b634727c2a726d12600d58c5d4c9ba77260fc
AndreyChenzov242 <andreychenzov@gmail.com>
2020-07-19 02:53:27 +0300

    last

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(function(){
 	$('.click').on('click', function(){
 		
 		$('.burger-menu').slideToggle(500);
-        $(".burger-menu").css("display", "flex");
+		$(".burger-menu").css("display", "flex");
  });
 	
 });
